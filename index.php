57cfe0f356ba8d02a78c68782f665031d75b4cb1
Omar Karim Martín Cornejo <omarkhd.mx@gmail.com>
2013-08-22 14:29:47 -0500

    added instructions to change the smvc system folder

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
+/*
+ * you can change the directory of the smvc system folder, just make sure to require
+ * the bootstrap.php file with the new correct path, in the line below this comment
+ */
 require_once __DIR__ . '/system/bootstrap.php';
 \smvc\controller\FrontController::run();
